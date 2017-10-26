7f393076a4c8d6cefd64043a3066653a2bb85e29
Petrie van Zyl <pvanzyl065@gmail.com>
2017-10-26 09:04:06 +0200

    started unit health bar

--- FantasyFighter/Assets/Scripts/Game/ObjectUI.cs
+++ FantasyFighter/Assets/Scripts/Game/ObjectUI.cs
@@ -5,20 +5,40 @@ using UnityEngine.UI;
 
 public class ObjectUI : MonoBehaviour {
     public int heightOffset = 40;
-    public GameObject UnitText;
+    // public GameObject UnitText;
+    public GameObject unitUI;
     
     GameObject[] wizards;
-    private List<GameObject> playerNameText = new List<GameObject>();
+
+    private List<GameObject> UnitUIList = new List<GameObject>();
+
+    private GameObject[] UnitText;
+    private GameObject[] UnitHealthBar;
+    private GameObject[] UnitHealthBarHandle;
         
 	void Awake () {
         wizards = GameObject.FindGameObjectsWithTag("Player");
-
+        UnitText = new GameObject[wizards.Length];
+        UnitHealthBar = new GameObject[wizards.Length];
+        UnitHealthBarHandle = new GameObject[wizards.Length];
 
         for (int x = 0; x < wizards.Length; x++) {
 
-            playerNameText.Add(Instantiate(UnitText, this.transform));
+            // playerNameText.Add(Instantiate(UnitText, this.transform));
+            // playerNameText[x].GetComponent<Text>().text = wizards[x].GetComponent<UnitScript>().ownerName;
 
-            playerNameText[x].GetComponent<Text>().text = wizards[x].GetComponent<UnitScript>().ownerName;
+            UnitUIList.Add(Instantiate(unitUI, this.transform));
+            
+            UnitText[x] = UnitUIList[x].transform.GetChild(0).gameObject;
+            UnitHealthBar[x] = UnitUIList[x].transform.GetChild(1).gameObject;
+            UnitHealthBarHandle[x] = UnitUIList[x].transform.GetChild(1).GetChild(0).GetChild(0).gameObject;
+
+            Scrollbar hp = UnitHealthBar[x].GetComponent<Scrollbar>();
+            ColorBlock cb = hp.colors;
+            cb.disabledColor = GameManager.GM.colors.color[x];
+            hp.colors = cb;
+
+            UnitText[x].GetComponent<Text>().text = wizards[x].GetComponent<UnitScript>().ownerName;
         }
 	}
 	
@@ -27,7 +47,10 @@ public class ObjectUI : MonoBehaviour {
         for (int x = 0; x < wizards.Length; x++) {
             playerPos = Camera.main.WorldToScreenPoint(wizards[x].transform.position);
             playerPos.y += heightOffset;
-            playerNameText[x].transform.position = playerPos;
+            UnitUIList[x].transform.position = playerPos;
+
+            UnitHealthBarHandle[x].GetComponent<RectTransform>().sizeDelta = new Vector2(50, 20); //wizards[unitNum].GetComponent<UnitScript>().ownerName;
         }
     }
+
 }
